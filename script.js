d1d3e9b0873990adc1ff8648ef25f840a27455eb
ProJeeter <maxlawson20007@gmail.com>
2025-02-03 14:13:13 +0000

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let jeetland;
+let jeetland, xyz;
+
+function preload(){
+    xyz = loadImage("abdur goofy.jpg");
+}
 
 function setup(){
-    createCanvas(windowWidth, windowHeight)
+    createCanvas(windowWidth, windowHeight);
+
+    jeetland = new Sprite(windowWidth/2, windowHeight/2, 50, 50, "s");
+    jeetland.img = xyz;
+    jeetland.scale = 0.25;
 
 }
 
 function draw(){
     background("pink")
+
+    jeetland.rotationSpeed = 0.5;
+    jeetland.speed = 2
 }
